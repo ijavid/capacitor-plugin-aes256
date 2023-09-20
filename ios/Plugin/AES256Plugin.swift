df0cc6171b828dacae826ee1cf8238d5af75934d
David Pszota <david.pszota@wellapp.com>
2023-09-20 15:19:17 +0200

    fis IOS swift syntax

--- ios/Plugin/AES256Plugin.swift
+++ ios/Plugin/AES256Plugin.swift
@@ -10,26 +10,26 @@ public class AES256Plugin: CAPPlugin {
     private static let PBKDF2_ITERATION_COUNT = 1001;
     private static let aes256Queue = DispatchQueue(label: "AESQUEUE", qos: DispatchQoS.background, attributes: .concurrent)
 
-    private let defaultKey = "";
-    private let defaultIv = "";
+    private var defaultKey = ""
+    private var defaultIv = ""
 
     override public func load() {
-        defaultKey = getConfigValue("secureKey") as? String
-        defaultIv = getConfigValue("secureIv") as? String
+        defaultKey = getConfig().getString("secureKey") ?? ""
+        defaultIv = getConfig().getString("secureIv") ?? ""
     }
 
     // Encrypts the plain text using aes256 encryption alogrithm
     @objc func encrypt(_ call: CAPPluginCall) {
-        let secureKey = call.getString("secureKey") ?? defaultKey ?? "";
-        let iv = call.getString("iv") ?? "" ?? defaultIv ?? "";
-        let value = call.getString("value") ?? "";
+        let secureKey = call.getString("secureKey") ?? defaultKey
+        let iv = call.getString("iv") ?? defaultIv
+        let value = call.getString("value") ?? ""
 
         if(secureKey.isEmpty || iv.isEmpty || value.isEmpty) {
             call.reject("Invalid parameters")
             return
         }
 
-        let encrypted = AES256CBC.encryptString(value, password: secureKey, iv: iv) ?? "";
+        let encrypted = AES256CBC.encryptString(value, password: secureKey, iv: iv) ?? ""
         call.resolve([
             "response": encrypted
         ])
@@ -37,9 +37,9 @@ public class AES256Plugin: CAPPlugin {
 
     // Decrypts the aes256 encoded string into plain text
     @objc func decrypt(_ call: CAPPluginCall) {
-        let secureKey = call.getString("secureKey") ?? defaultKey ?? "";
-        let iv = call.getString("iv") ?? "" ?? defaultIv ?? "";
-        let value = call.getString("value") ?? "";
+        let secureKey = call.getString("secureKey") ?? defaultKey
+        let iv = call.getString("iv") ?? defaultIv
+        let value = call.getString("value") ?? ""
 
         if(secureKey.isEmpty || iv.isEmpty || value.isEmpty) {
             call.reject("Invalid parameters")
@@ -55,12 +55,12 @@ public class AES256Plugin: CAPPlugin {
 
     // Generates the secure key from the given password
     @objc func generateSecureKey(_ call: CAPPluginCall) {
-        let password = call.getString("password") ?? "";
+        let password = call.getString("password") ?? ""
         if(password.isEmpty) {
             call.reject("Invalid parameters")
             return
         }
-        let secureKey = PBKDF2.pbkdf2(hash:CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1), password:password, salt:AES256CBC.generateSalt(), keyByteCount:AES256.SECURE_KEY_LENGTH, rounds:AES256.PBKDF2_ITERATION_COUNT) ?? "";
+        let secureKey = PBKDF2.pbkdf2(hash:CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1), password:password, salt:AES256CBC.generateSalt(), keyByteCount:AES256Plugin.SECURE_KEY_LENGTH, rounds:AES256Plugin.PBKDF2_ITERATION_COUNT) ?? "";
         call.resolve([
             "response": secureKey
         ])
@@ -68,12 +68,12 @@ public class AES256Plugin: CAPPlugin {
 
     // Generates the IV from the given password
     @objc func generateSecureIv(_ call: CAPPluginCall) {
-        let password = call.getString("password") ?? "";
+        let password = call.getString("password") ?? ""
         if(password.isEmpty) {
             call.reject("Invalid parameters")
             return
         }
-        let secureIV = PBKDF2.pbkdf2(hash:CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1), password:password, salt:AES256CBC.generateSalt(), keyByteCount:AES256.SECURE_IV_LENGTH, rounds:AES256.PBKDF2_ITERATION_COUNT) ?? "";
+        let secureIV = PBKDF2.pbkdf2(hash:CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1), password:password, salt:AES256CBC.generateSalt(), keyByteCount:AES256Plugin.SECURE_IV_LENGTH, rounds:AES256Plugin.PBKDF2_ITERATION_COUNT) ?? "";
         call.resolve([
             "response": secureIV
         ])
